2e54860622c8859bfb0b07e67e08b70ce52d405e
Divya <kaushikdivya16@gmail.com>
2019-02-04 10:28:06 -0800

    Adding Singleton class example

---
+++ Singleton.py
@@ -0,0 +1,9 @@
+class Singleton(type):
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls._instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__init__(*args, **kwargs)
+        return cls._instance
